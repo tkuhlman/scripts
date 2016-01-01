3cb230cdd92f80527a6933beb68ed2783a59f1ad
Timothy Kuhlman <timothy.kuhlman@canonical.com>
2015-12-31 17:03:30 -0700

    Added better options and ip address handling to the anonymize script

--- log/anonymize_web_log_ips.py
+++ log/anonymize_web_log_ips.py
@@ -2,40 +2,60 @@
 """ Takes an apache web log and replaces ips with a number to allows for anonymized analysis.
     Optionally some ips can be striped also.
 """
+import argparse
+import netaddr
 import sys
 
-def skip_ip(ip, skip_ips):
-    """ Return true if ip startswith any of the ips in the skip_ips list """
-    for skip_ip in skip_ips:
-        if ip.startswith(skip_ip):
-            return True
 
-    return False
+def main():
+    # Parse arguments, build skip_set and paths as needed
+    parser = argparse.ArgumentParser()
+    parser.add_argument('log_path', help='The path to the log to be anonymized')
+    parser.add_argument('--output', '-o', help='Output file, defaults to input file'
+                                               'with .anonymized extension')
+    parser.add_argument('--skip', '-s', action='append', help='A cidr notation network for which any'
+                                                       'for which any source traffic will be removed'
+                                                       'from the final output. Can be specified multiple'
+                                                       'times')
+    parser.add_argument('--skip_private', action='store_true', default=False,
+                        help='If specified remove any private source ips from the final file')
+    parser.add_argument('--verbose', '-v', action='store_true', default=False,
+                        help='Verbose: output lines processed/skipped')
+    args = parser.parse_args()
 
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv
+    if args.skip is not None:
+        skip_ips = netaddr.IPSet()
+        for cidr in args.skip:
+            skip_ips.add(cidr)
+    else:
+        skip_ips = None
+    if args.output is None:
+        anonymized_path = args.log_path + '.anonymized'
+    else:
+        anonymized_path = args.output
 
-    skip_ips = None
-    if len(argv) < 2:
-        print('Usage: {} <path to web log with ip as first component> [ip prefix to skip]')
-    elif len(argv) > 2:
-        skip_ips = sys.argv[2:]
-
+    # Process the lines of the file
     ips = {}
-    with open(argv[1] + '.anonymized', 'w') as new_file:
-        with open(argv[1], 'r') as log_file:
+    total = 0
+    skip = 0
+    with open(anonymized_path, 'w') as new_file:
+        with open(args.log_path, 'r') as log_file:
             for line in log_file:
+                total += 1
                 splits = line.split()
-                ip = splits[0]
-                if skip_ips is not None and skip_ip(ip, skip_ips):
+                ip = netaddr.IPAddress(splits[0])
+                if (args.skip_private and ip.is_private()) or (skip_ips is not None and ip in skip_ips):
+                    skip += 1
                     continue
 
-                if not ips.has_key(ip):
-                    ips[ip] = str(len(ips))
+                if not ips.has_key(str(ip)):
+                    ips[str(ip)] = str(len(ips))
 
-                anonymized_line = "{} {}\n".format(ips[ip], ' '.join(splits[1:]))
+                anonymized_line = "{} {}\n".format(ips[str(ip)], ' '.join(splits[1:]))
                 new_file.write(anonymized_line)
+
+    if args.verbose:
+        print('Total lines processed {}, skipped {}'.format(total, skip))
 
 
 if __name__ == "__main__":
